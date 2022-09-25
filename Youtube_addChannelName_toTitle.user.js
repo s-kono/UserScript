fbdf29cce18e441365a378c0fe5a1dd6851fde49
s-kono <s-kono@outlook.com>
2022-09-25 21:28:09 +0900

    .

--- Youtube_addChannelName_toTitle.user.js
+++ Youtube_addChannelName_toTitle.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name           Youtube_addChannelName_toTitle
 // @description    Youtube : add channelname to title
-// @version        0.20220925.2
+// @version        0.20220925.3
 // @namespace      https://github.com/s-kono/UserScript
 // @author         github.com/s-kono
-// @match          https://www.youtube.com/watch*
+// @match          https://www.youtube.com/*
 // @grant          none
 // @run-at         document-idle
 // @icon           https://www.youtube.com/s/desktop/435d54f2/img/favicon_144x144.png
@@ -16,7 +16,8 @@
 (function() {
     'use strict';
     let pre_title = '';
-    const timer = setInterval(function() {
+
+    function addChannelName() {
         for(const ch_name of $('#upload-info yt-formatted-string.ytd-channel-name')) {
             const head_title = $('head > title')[0];
             const a = head_title.innerText;
@@ -24,11 +25,27 @@
                 continue;
             }
             const b = String(ch_name.innerText) + ": " + String(head_title.innerText);
-            if(a != b) {
-                head_title.innerText = b;
-                pre_title = b;
+            if(a == b) {
+                continue;
             }
-            //clearInterval(timer);
+            head_title.innerText = b;
+            pre_title = b;
         }
-    }, 3000);
+    }
+
+    setTimeout(function() {
+        const title_observer = new MutationObserver(function (mutations) {
+            if (location.href.match(/\/watch/)) {
+                setTimeout(function() {
+                    // title_observer.disconnect();
+                    addChannelName();
+                    // title_observer.observe(document.querySelector('title'), { childList: true, subtree: false, characterData: false, attributes: false });
+                }, 2000);
+            }
+        });
+        title_observer.observe(document.querySelector('title'), { childList: true, subtree: false, characterData: false, attributes: false });
+        if (location.href.match(/\/watch/)) {
+            addChannelName();
+        }
+    }, 4000);
 })();
