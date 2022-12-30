e58df90df97fc64f36cf73183b85569754130822
s-kono <s-kono@outlook.com>
2022-12-31 02:22:18 +0900

    .

--- Gmail_ModCSS.user.js
+++ Gmail_ModCSS.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name           Gmail_ModCSS
 // @description    Gmail ModCSS
-// @version        0.20220903.0
+// @version        0.20221231.0
 // @namespace      https://github.com/s-kono/UserScript
 // @author         github.com/s-kono
-// @match          https://mail.google.com/mail/u/*
+// @match          https://mail.google.com/mail/u/0/*
 // @grant          none
 // @run-at         document-idle
 // @icon           https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/1024px-Gmail_icon_%282020%29.svg.png
@@ -15,46 +15,9 @@
 (function() {
     'use strict';
     const css = `
-table, .ajA, .iY .Bu, .ii div {
-    color: #fff !important;
-    background: #000 !important;
-    background-color: #000 !important;
-}
-table.cf, .UG, .nr {
-    color: #fff !important;
-    background-color: #222 !important;
-}
-.adp, .h9, .adI, .aHn {
-    background-color: #fc3 !important;
-}
-.gb_Se.gb_Te .gb_df {
-    color: #fff !important;
-}
-.gb_df {
-    padding: 0   0 8px 3px !important;
-    margin: -5px 0 8px 3px !important;
-    color: #999 !important;
-}
-.ig .g3, .hI .g3, .iv .g3, .hx .hb, .go, .gt a {
-    color: #ba0 !important;
-}
-.hP, .hx, .gD, .ams, .gs div, .gs p, .gs h1, .gb_Re.gb_Se .gb_cf {
-    color: #fff !important;
-}
-.v .fY, .fY div, .AO .aeF .nH .v9 div {
-    background: #111 !important;
-    color: #eee !important;
-}
-.Ze {
-    background: #555 !important;
-    background-color: #555 !important;
-}
-table.gstt {
-    height: 30px !important;
-    margin: 8px 0 0 0 !important;
-}
-.gb_cf:not(.gb_tf) {
-    padding: 0 0 18px 5px !important;
+div.AO {
+ /* mix-blend-mode: soft-light; */
+    mix-blend-mode: exclusion;
 }
     `;
     const style = document.createElement('style');
