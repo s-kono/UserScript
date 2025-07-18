50ad9f6c30ea974912c76e134e45f6333a25a58f
s-kono <s-kono@outlook.com>
2025-07-18 21:36:29 +0900

    .

--- kb.vmware.com_CSVmod.user.js
+++ kb.vmware.com_CSVmod.user.js
@@ -2,8 +2,9 @@
 // @name           kb.vmware.com_CSVmod
 // @namespace      github.com/s-kono
 // @description    kb.vmware.com: fix Article margin
-// @version        0.20230305.0
+// @version        0.20230417.0
 // @match          https://kb.vmware.com/*
+// @match          https://web.archive.org/web/*/https://kb.vmware.com/s/article/*
 // @icon           https://www.google.com/s2/favicons?sz=64&domain=vmware.com
 // @grant          none
 // @run-at         document-end
@@ -23,3 +24,4 @@
     style.appendChild(document.createTextNode(css));
     document.head.appendChild(style);
 })();
+
